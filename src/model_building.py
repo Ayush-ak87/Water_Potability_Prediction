fd88aef1bf539969e9da6c93dca84ddc3949191c
Ayush <akagarwal.111a@gmail.com>
2024-12-08 16:50:14 +0530

    Model Building

---
+++ src/model_building.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import os
+import pickle
+from sklearn.ensemble import RandomForestClassifier
+
+train_data = pd.read_csv("./data/processed/train_processed.csv")
+
+x_train = train_data.iloc[:,0:-1].values
+y_train = train_data.iloc[:,-1].values
+
+model = RandomForestClassifier()
+model.fit(x_train,y_train)
+
+pickle.dump(model,open("model.pkl","wb"))
