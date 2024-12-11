9796793e52c884b36fae2f04b24935f28419784c
Ayush <akagarwal.111a@gmail.com>
2024-12-11 14:38:39 +0530

    Model Building Changes

--- src/model_building.py
+++ src/model_building.py
@@ -6,8 +6,11 @@ from sklearn.ensemble import RandomForestClassifier
 
 train_data = pd.read_csv("./data/processed/train_processed.csv")
 
-x_train = train_data.iloc[:,0:-1].values
-y_train = train_data.iloc[:,-1].values
+# x_train = train_data.iloc[:,0:-1].values
+# y_train = train_data.iloc[:,-1].values
+
+x_train = train_data.drop(columns=["Potability"], axis=1)
+y_train = train_data["Potability"]
 
 model = RandomForestClassifier()
 model.fit(x_train,y_train)
